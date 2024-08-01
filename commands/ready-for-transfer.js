c7b52e3417d6156c6942943a7de4d2271fb19b80
Miroslav Bajtoš <oss@bajtos.net>
2024-08-01 14:10:17 +0200

    feat: change ready-to-transfer output to CSV
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- commands/ready-for-transfer.js
+++ commands/ready-for-transfer.js
@@ -3,11 +3,12 @@ import { formatEther } from 'ethers'
 
 export const readyForTransfer = async opts => {
   const { contract } = await createContract(opts)
+  console.log('address,fil_amount')
   for (let i = 0; ; i++) {
     try {
       const address = await contract.readyForTransfer(i)
       const amount = await contract.rewardsScheduledFor(address)
-      console.log(`${address} FIL ${formatEther(amount)}`)
+      console.log(`${address},${formatEther(amount)}`)
     } catch (err) {
       break
     }
