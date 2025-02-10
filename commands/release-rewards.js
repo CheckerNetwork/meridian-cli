e85dcfc32cf6aff15913a445aac724989c0198b7
Julian Gruber <julian@juliangruber.com>
2025-02-10 14:58:27 +0100

    clean up

--- commands/release-rewards.js
+++ commands/release-rewards.js
@@ -1,6 +1,4 @@
 import { createContract } from '../index.js'
-import { relative, join } from 'node:path'
-import { fileURLToPath } from 'node:url'
 import pRetry from 'p-retry'
 
 export const releaseRewards = async opts => {
@@ -11,16 +9,4 @@ export const releaseRewards = async opts => {
   console.log('Awaiting confirmation...')
   await pRetry(() => tx.wait())
   console.log('Rewards released')
-
-  const bin = relative(
-    process.cwd(),
-    fileURLToPath(new URL('.', import.meta.url))
-  )
-  console.log('For progress, check')
-  console.log()
-  console.log(`  $ node ${join(bin, 'participant-count-scheduled-for-transfer.js')}`)
-  console.log()
-  console.log(`  $ node ${join(bin, 'scheduled-for-transfer.js')}`)
-  console.log()
-  process.exit()
 }
